cb6e28971b93375499e03fb68aaad658199e69cf
Artan Thaqi <artaan.thaqi@gmail.com>
2023-11-15 21:46:14 +0100

    Python script with panda library

---
+++ Script.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+# Load the dataset
+file_path = "Crime_Data_from_2020_to_Present_20231113.csv"
+df = pd.read_csv(file_path)
+
+# Check for null values and visualize
+null_counts = df.isnull().sum()
+duplicate_count = df.duplicated().sum()
+
+print(f"Number of null rows: '{null_counts}' | Number of duplicate rows: {duplicate_count}")
