a7f5fcb8b17587bd53a5b69c789e3d2af1e1ffe6
Artan Thaqi <artaan.thaqi@gmail.com>
2023-11-17 13:27:53 +0100

    Formatting the text in console

--- Script.py
+++ Script.py
@@ -9,7 +9,7 @@ df = pd.read_csv(file_path)
 null_counts = df.isnull().sum()
 duplicate_count = df.duplicated().sum()
 
-print(f"Number of null rows \n'{null_counts}' \nNumber of duplicate rows: {duplicate_count}")
+print(f"Number of null rows \n'{null_counts}' \nNumber of duplicate rows: {duplicate_count} \n")
 
 # Visualizing the amount of null & duplicate values for each column in the dataset
 plt.figure(figsize = (12, 6))
@@ -23,7 +23,7 @@ plt.legend()
 plt.show()
 
 # Print the percentage and number of null values for specified columns
-visualization_columns = ['Vict Sex', 'Vict Descent', 'DATE OCC', 'AREA NAME']
+visualization_columns = ['Vict Sex', 'Vict Descent', 'DATE OCC', 'AREA NAME', 'Vict Age']
 
 for col in visualization_columns:
     null_percentage = (df[col].isnull().sum() / len(df)) * 100
@@ -34,11 +34,15 @@ for col in visualization_columns:
 df['Vict Sex'].fillna('X', inplace=True)
 df['Vict Descent'].fillna('X', inplace=True)
 
+# Count the number of rows with 'Vict Age' as 0
+rows_age= len(df[df['Vict Age'] == 0])
+
+# Rows with 'Vict Age' as 0
+print(f"\nNumber of rows with 'Vict Age' as 0: {rows_age}")
+
 # Calculate average age excluding rows where 'Vict Age' is 0
 average_age = df[df['Vict Age'] != 0]['Vict Age'].mean()
 
-# Count the number of rows affected
-rows_affected = len(df[df['Vict Age'] == 0])
 
 # Fill null values and rows with 'Vict Age' as 0 with the calculated average
 df['Vict Age'].replace(0, pd.NA, inplace = True)
@@ -46,7 +50,7 @@ df['Vict Age'].fillna(average_age, inplace = True)
 
 # Print the average age and the number of rows affected
 print(f"\nAverage Age: {average_age:.2f}")
-print(f"Number of rows with 'Vict Age' as 0: {rows_affected}")
+
 
 # Convert 'DATE OCC' to datetime format
 df['DATE OCC'] = pd.to_datetime(df['DATE OCC'], errors='coerce')
@@ -59,5 +63,4 @@ df = df[(df['DATE OCC'].dt.year >= 2020) & (df['DATE OCC'].dt.year <= 2022)]
 removed_rows = initial_rows - len(df)
 removed_percentage = (removed_rows / initial_rows) * 100
 
-print(f"\nRows removed based on 'DATE OCC' range (2020-2022): {removed_rows} | Percentage of removed rows: {removed_percentage:.2f}%")
-
+print(f"\nRows removed based on 'DATE OCC' range (2020-2022): {removed_rows} \nPercentage of removed rows: {removed_percentage:.2f}%")
